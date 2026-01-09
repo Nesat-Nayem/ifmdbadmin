5bd0fdd73797783b179a6d141d7970d6bb0727d4
Nesat Nayem <nesatnayem042@gmail.com>
2026-01-09 07:36:09 +0600

    root url update

--- src/store/eventsBookings.ts
+++ src/store/eventsBookings.ts
@@ -67,7 +67,7 @@ interface EventesTicketBookingsResponse {
 export const eventesTicketBookingsAPI = createApi({
   reducerPath: 'eventesTicketBookingsAPI',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:8080/v1/api',
+    baseUrl: 'https://api.moviemart.org/v1/api',
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as IRootState).auth.token
       if (token) {
